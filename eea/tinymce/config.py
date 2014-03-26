d95c71fcf538e3622cca2e834e15f0004db11cda
Olimpiu Rob <olimpiu.rob@gmail.com>
2014-03-26 17:04:07 +0200

    Refs #18641 EEA; Initial commit

---
+++ eea/tinymce/config.py
@@ -0,0 +1,7 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'eea.tinymce'
+
+from zope.i18nmessageid import MessageFactory
+EEAMessageFactory = MessageFactory('eea')
---
+++ eea/tinymce/interfaces.py
@@ -0,0 +1,8 @@
+""" Public interfaces
+"""
+# Control Panel
+from eea.tinymce.controlpanel.interfaces import ISettings
+
+__all__ = [
+    ISettings.__name__,
+]
