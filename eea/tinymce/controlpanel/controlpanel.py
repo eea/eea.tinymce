415e6c494d8f5b359dbaaefd2c959747a8a82b49
Olimpiu Rob <olimpiu.rob@gmail.com>
2014-03-27 17:05:28 +0200

    Refs #18641 EEA; Added plugins

--- eea/tinymce/controlpanel/controlpanel.py
+++ eea/tinymce/controlpanel/controlpanel.py
@@ -10,17 +10,39 @@ class EEACharPlugin(object):
     """ EEACharPlugin  Settings Section
     """
     implements(ITinyMCEPlugin)
-    prefix = 'eeacharplugin'
-    title = 'EEACharPlugin settings'
+    prefix = 'eeacharlimit'
+    title = 'EEA CharLimit'
 
     def __init__(self):
         self.form_fields = FormFields(
             schema.List(
-                __name__='EEA Char plugin',
-                title=_(u"EEA Char plugin for Portal Types"),
-                description=_(u"EEA Char plugin will be enabled for the\
+                __name__='EEA CharLimit plugin',
+                title=_(u"EEA CharLimit for Portal Types"),
+                description=_(u"EEA CharLimit plugin will be enabled for the\
                  following content types"),
                 required=False,
+                value_type=schema.Choice(
+                    vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes")
+                )
+            )
+
+
+class EEAToggleFullscreen(object):
+    """ EEA Toggle Fullscreen  Settings Section
+    """
+    implements(ITinyMCEPlugin)
+    prefix = 'eeatogglefullscreen'
+    title = 'EEA Toggle Fullscreen'
+
+    def __init__(self):
+        self.form_fields = FormFields(
+            schema.List(
+                __name__='EEA Toggle Fullscreen',
+                title=_(u"Fullscreen for Portal Types"),
+                description=_(u"TinyMCE will go fullscreen when clicking on\
+                 the body of the text fields of the following content-types"),
+                required=False,
+                default=[u"Document"],
                 value_type=schema.Choice(
                     vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes")
                 )
---
+++ eea/tinymce/skins/eea_tinymce_plugins/eeatogglefullscreen/editor_plugin.js
@@ -0,0 +1,91 @@
+(function () {
+
+    function triggerEvent (node, type) {
+        var evt;
+        if (document.createEvent) {
+            evt = document.createEvent('MouseEvents');
+            evt.initEvent(type, true, false);
+            node.dispatchEvent(evt);
+        } else if (document.createEventObject) {
+            node.fireEvent('on' + type);
+        }
+    }
+
+    var debounce = function (func, threshold, execAsap) {
+        //http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+        var timeout;
+        return function debounced () {
+            var obj = this;
+
+            function delayed () {
+                if (!execAsap)
+                    func.apply(obj, arguments);
+                timeout = null;
+            }
+
+            if (timeout)
+                clearTimeout(timeout);
+            else if (execAsap)
+                func.apply(obj, arguments);
+
+            timeout = setTimeout(delayed, threshold || 100);
+        };
+
+    };
+
+    tinymce.create("tinymce.plugins.EEAToggleFullScreenPlugin", {
+        init: function (d) {
+            ed = d;
+            var tinymce_container = document.getElementById('mce_fullscreen_container'),
+                $tinymce_container,
+                setHeight,
+                debouncedSetHeight;
+
+            if (tinymce_container) {
+                tinymce_container.onclick = function (e) {
+                    if (e.target.id === "mce_fullscreen_parent") {
+                        var fullscreen_button = document.getElementById('mce_fullscreen_fullscreen');
+                        tinymce_container.onclick = null;
+                        triggerEvent(fullscreen_button, 'click');
+                    }
+                };
+                $tinymce_container = $(tinymce_container);
+
+                setHeight = function (evt) {
+                    $tinymce_container.find('iframe').css({ 'height': $tinymce_container.height() - 85});
+                };
+                debouncedSetHeight = debounce(setHeight, 200, false);
+
+                window.addEventListener('resize', debouncedSetHeight);
+            }
+
+            ed.onLoadContent.add(this.loadContent, this);
+
+        },
+
+        getInfo: function () {
+            return {
+                longname: "EEA Toggle Fullscreen",
+                author: "Moxiecode Systems AB",
+                authorurl: "http://tinymce.moxiecode.com",
+                infourl: "http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/fullscreen",
+                version: tinymce.majorVersion + "." + tinymce.minorVersion
+            }
+        },
+        loadContent: function (ed) {
+            var body = ed.getBody();
+            var container = ed.getContainer();
+            if (ed.getParam('fullscreen_for')) {
+                if (!document.getElementById('mce_fullscreen_fullscreen')) {
+                    $(body).click(function () {
+                        var fullscreen_button = container.querySelector('.mceButton.mce_fullscreen');
+                        if (!document.getElementById('mce_fullscreen_fullscreen')) {
+                            triggerEvent(fullscreen_button, 'click');
+                        }
+                    });
+                }
+            }
+        }
+    });
+    tinymce.PluginManager.add("eeatogglefullscreen", tinymce.plugins.EEAToggleFullScreenPlugin);
+})();
