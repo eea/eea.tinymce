8e787885dea7c0ea8ad86243e2320b75914655c0
ichim-david <ichim.david@gmail.com>
2014-08-05 13:03:06 +0300

    Refs #20461; added the required settings for the template plugin in order to display templates on the plugin popup

---
+++ eea/tinymce/skins/eea_tinymce_plugins/eeatemplateinit/editor_plugin.js
@@ -0,0 +1,30 @@
+// jslint:disable
+/*global jQuery, tinymce, eeatinymceplugins, portal_url */
+
+
+(function () {
+    tinymce.create("tinymce.plugins.EEATemplateInitPlugin", {
+        init: function (ed) {
+            ed.onLoadContent.add(this.loadContent, this);
+        },
+
+        getInfo: function () {
+            return {
+                longname: "EEA Template Init",
+                author: "David Ichim",
+                authorurl: "http://tinymce.moxiecode.com",
+                infourl: "http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/template",
+                version: tinymce.majorVersion + "." + tinymce.minorVersion
+            }
+        },
+
+        loadContent: function (ed) {
+            var tinyMCETemplateList = [
+                {title: "Figure", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/figure.html", decription:"Figure title and image"},
+            ];
+            ed.settings.template_templates = tinyMCETemplateList;
+        }
+    });
+    tinymce.PluginManager.add("eeatemplateinit", tinymce.plugins.EEATemplateInitPlugin);
+
+}());
