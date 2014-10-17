7d01ae564176ae8c5a65b02434311fe3de7380da
ichim-david <ichim.david@gmail.com>
2014-10-17 13:02:43 +0300

    Refs #21327; added eeapluginsutils.js with methods that can be used by our plugins in order to avoid code duplication. Added function that checks if eeatinymceplugins json is loaded

---
+++ eea/tinymce/skins/eea_tinymce_plugins/eeapluginsutils.js
@@ -0,0 +1,45 @@
+//http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+var debounce = function (func, threshold, execAsap) {
+    var timeout;
+
+    var obj = this;
+    return function debounced () {
+
+        function delayed () {
+            if (!execAsap) {
+                func.apply(obj, arguments);
+            }
+            timeout = null;
+        }
+
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        else if (execAsap) {
+            func.apply(obj, arguments);
+        }
+        timeout = setTimeout(delayed, threshold || 100);
+    };
+
+};
+
+var hasEEATinyMCESettings = function() {
+    var eeatinymceplugins = window.eeatinymceplugins || "";
+    var missing_settings_message;
+    if (!eeatinymceplugins) {
+        (function(){
+            missing_settings_message = "Couldn't load tinymceplugins.json";
+            window.console ?
+                window.console.log(missing_settings_message) :
+                window.alert(missing_settings_message);
+            return false;
+        }());
+    }
+    return true;
+};
+
+window.EEAPluginsUtils = {
+    "version": 1.0,
+    'debounce': debounce,
+    'hasEEATinyMCESettings': hasEEATinyMCESettings
+};
--- eea/tinymce/skins/eea_tinymce_plugins/eeareadabilitychecker/editor_plugin.js
+++ eea/tinymce/skins/eea_tinymce_plugins/eeareadabilitychecker/editor_plugin.js
@@ -1,109 +1,92 @@
 // jslint:disable
-/*global $, tinymce, portal_url, window */
+/*global $, tinymce, portal_url, window, EEAPluginsUtils */
 
 (function () {
-    var debounce = function (func, threshold, execAsap) {
-        var timeout;
-
-        var obj = this;
-        return function debounced () {
-
-            function delayed () {
-                if (!execAsap) {
-                    func.apply(obj, arguments);
-                }
-                timeout = null;
-            }
-
-            if (timeout) {
-                clearTimeout(timeout);
-            }
-            else if (execAsap) {
-                func.apply(obj, arguments);
-            }
-            timeout = setTimeout(delayed, threshold || 100);
-        };
-
-    };
-
     tinymce.create("tinymce.plugins.EEAReadabilityChecker", {
-        init: function (ed, url) {
-            var css_url = portal_url + '/eeareadabilitychecker.css';
-            tinymce.DOM.loadCSS(css_url);
-
-            ed.onInit.add(function() {
-
-                var $container = $(ed.getContainer());
-                var $char_limit_row = $container.find('.charlimit-row');
-                var $char_limit = $char_limit_row.children();
-                if ($char_limit_row.length < 1) {
+        init: function (ed) {
+            $.ajaxSetup({
+                cache: true
+            });
+            $.getScript(portal_url + '/eeapluginsutils.js', function() {
+                if (!EEAPluginsUtils.hasEEATinyMCESettings()) {
                     return;
                 }
-                if ($char_limit.is(':empty')) {
-                    $char_limit.remove();
-                    $char_limit_row.addClass('fullwidth');
-                }
+                var css_url = portal_url + '/eeareadabilitychecker.css';
+                tinymce.DOM.loadCSS(css_url);
 
-                var $el =$("<div class='readabilityChecker charlimit-info'><span class='eea-icon eea-icon-question-circle eea-icon-lg'></span>Readability score <span class='readabilityValue'></span> <span class='readabilityLevel'></span></div>") ;
-                $el.appendTo($char_limit_row);
-                if ($char_limit.hasClass("charlimit-exceeded")) {
-                    $el.addClass("charlimit-expanded");
-                }
-                $el.click(function() {
-                    ed.focus();
-                    var char_info = $char_limit_row.find('.charlimit-info').eq(0);
-                    var eea_char_count = parseInt(char_info.text().match("[0-9]+")[0], 10);
-                    ed.windowManager.open({
-                        file: portal_url + "/portal_skins/eeareadabilitychecker/eeareadabilitychecker",
-                        width: 560,
-                        height: 650,
-                        inline: 1
-                    },
-                    {
-                        textstatistics: window.textstatistics,
-                        charCount: eea_char_count
-                    });
-                });
-                var $readability_value = $el.find($(".readabilityValue"));
-                var $readability_level = $el.find($(".readabilityLevel"));
-
-                var setReadabilityValue = function() {
-                    var text = ed.getContent();
-                    if (!text) {
-                        $readability_value.html(0);
-                        $readability_level.text("");
-                        $el.addClass('charlimit-info');
+                ed.onInit.add(function() {
+                    var $container = $(ed.getContainer());
+                    var $char_limit_row = $container.find('.charlimit-row');
+                    var $char_limit = $char_limit_row.children();
+                    if ($char_limit_row.length < 1) {
                         return;
                     }
-                    var text_count_obj = window.textstatistics(text);
-                    var grade = text_count_obj.fleschKincaidReadingEase();
-                    $readability_value.html(grade);
-                    if (grade < 30) {
-                        $el.attr('class', 'readabilityChecker charlimit-info charlimit-exceeded');
-                        $readability_level.text("(low)");
-                    } else if (grade < 70) {
-                        $el.attr('class', 'readabilityChecker charlimit-info charlimit-warn');
-                        $readability_level.text("(average)");
-                    } else {
-                        $el.attr('class', 'readabilityChecker charlimit-info');
-                        $readability_level.text("(high)");
+                    if ($char_limit.is(':empty')) {
+                        $char_limit.remove();
+                        $char_limit_row.addClass('fullwidth');
                     }
-                    if ($char_limit.hasClass("charlimit-exceeded") || $char_limit.hasClass("charlimit-warn")) {
+
+                    var $el =$("<div class='readabilityChecker charlimit-info'><span class='eea-icon eea-icon-question-circle eea-icon-lg'></span>Readability score <span class='readabilityValue'></span> <span class='readabilityLevel'></span></div>") ;
+                    $el.appendTo($char_limit_row);
+                    if ($char_limit.hasClass("charlimit-exceeded")) {
                         $el.addClass("charlimit-expanded");
                     }
-                };
+                    $el.click(function() {
+                        ed.focus();
+                        var char_info = $char_limit_row.find('.charlimit-info').eq(0);
+                        var eea_char_count = parseInt(char_info.text().match("[0-9]+")[0], 10);
+                        ed.windowManager.open({
+                                file: portal_url + "/portal_skins/eeareadabilitychecker/eeareadabilitychecker",
+                                width: 560,
+                                height: 650,
+                                inline: 1
+                            },
+                            {
+                                textstatistics: window.textstatistics,
+                                charCount: eea_char_count
+                            });
+                    });
+                    var $readability_value = $el.find($(".readabilityValue"));
+                    var $readability_level = $el.find($(".readabilityLevel"));
 
-                setReadabilityValue();
-                // recalculate score value on keyUp every 1.2sec
-                ed.onKeyUp.add(debounce(function() {
-                    return setReadabilityValue();
-                }, 1200, false));
-                // recalculate score value when content is set
-                // such as the moment when you paste markup within
-                // the html plugin
-                ed.onSetContent.add(debounce(function() {
-                    return setReadabilityValue();
-                }, 500, false));
+                    var setReadabilityValue = function() {
+                        var text = ed.getContent();
+                        if (!text) {
+                            $readability_value.html(0);
+                            $readability_level.text("");
+                            $el.addClass('charlimit-info');
+                            return;
+                        }
+                        var text_count_obj = window.textstatistics(text);
+                        var grade = text_count_obj.fleschKincaidReadingEase();
+                        $readability_value.html(grade);
+                        if (grade < 30) {
+                            $el.attr('class', 'readabilityChecker charlimit-info charlimit-exceeded');
+                            $readability_level.text("(low)");
+                        } else if (grade < 70) {
+                            $el.attr('class', 'readabilityChecker charlimit-info charlimit-warn');
+                            $readability_level.text("(average)");
+                        } else {
+                            $el.attr('class', 'readabilityChecker charlimit-info');
+                            $readability_level.text("(high)");
+                        }
+                        if ($char_limit.hasClass("charlimit-exceeded") || $char_limit.hasClass("charlimit-warn")) {
+                            $el.addClass("charlimit-expanded");
+                        }
+                    };
+
+                    setReadabilityValue();
+                    // recalculate score value on keyUp every 1.2sec
+                    ed.onKeyUp.add(utils.debounce(function() {
+                        return setReadabilityValue();
+                    }, 1200, false));
+                    // recalculate score value when content is set
+                    // such as the moment when you paste markup within
+                    // the html plugin
+                    ed.onSetContent.add(utils.debounce(function() {
+                        return setReadabilityValue();
+                    }, 500, false));
+                });
             });
         },
 
