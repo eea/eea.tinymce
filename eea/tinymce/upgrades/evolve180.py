f7ce813649ad95a6826bcc1d355fdf47af154ad4
ichim-david <ichim.david@gmail.com>
2014-10-21 18:07:49 +0300

    Refs #21327; added upgrade which modifies the char_limit settings to set the field value as a key and it's plugins as a dict value

--- eea/tinymce/upgrades/evolve180.py
+++ eea/tinymce/upgrades/evolve180.py
@@ -1,8 +1,34 @@
 """ EEA.Tinymce 180 upgrade steps
 """
+from Products.TinyMCE.interfaces.utility import ITinyMCE
+import json
+from zope.component import queryUtility
 
 
 def evolve_settings(setuptool):
     """ Change the tinymceplugin.json output
     """
-    pass
+    utility = queryUtility(ITinyMCE)
+    plugin_settings = getattr(utility, 'eea_plugin_settings', {})
+    charlimit_settings = plugin_settings.get('eeacharlimit')
+    if not charlimit_settings:
+        return
+    char_settings = charlimit_settings.get('eea_char_limit')
+    if not char_settings:
+        return
+    json_settings = json.loads(char_settings[1:-1])
+    for item in json_settings:
+        new_list = []
+        value_list = item['settings']
+        for value in value_list:
+            if not value.get('fieldname'):
+                continue
+            fieldname = value.pop('fieldname')
+            new_list.append({fieldname: value})
+        item['settings'] = new_list
+    charlimit_settings['eea_char_limit'] = json.dumps(json_settings)
+    return True
+
+
+
+
