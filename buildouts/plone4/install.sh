6467fad8bac08cc230b4d97e9d2a7865293403e7
Olimpiu Rob <olimpiu.rob@gmail.com>
2015-03-09 11:18:30 +0200

    Refs #22402; Switched to curl in the jenkins build install script

--- buildouts/plone4/install.sh
+++ buildouts/plone4/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget -O bootstrap.sh https://raw.githubusercontent.com/eea/eea.plonebuildout.core/master/install.sh
+curl -o "bootstrap.sh" -k https://raw.githubusercontent.com/eea/eea.plonebuildout.core/master/install.sh
 chmod u+x bootstrap.sh
 ./bootstrap.sh
