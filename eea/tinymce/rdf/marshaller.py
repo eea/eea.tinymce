3994298926248637364bb9f592249ed0912109f0
David Ichim <ichim.david@gmail.com>
2017-03-16 17:23:18 +0100

    Refs #82316 cleanup of marshaller.py wip code and pylint fixes

--- eea/tinymce/rdf/marshaller.py
+++ eea/tinymce/rdf/marshaller.py
@@ -1,16 +1,15 @@
 """ eea.rdfmarshaller customizations for eea.tinymce
 """
 import surf
-import rdflib
 from eea.rdfmarshaller.interfaces import ISurfResourceModifier
 from eea.relations.content.interfaces import IBaseObject
 from zope.annotation import IAnnotations
 from zope.component import adapts, getMultiAdapter
-from zope.interface import Interface, implements
+from zope.interface import implements
 
 
 class Readability2SurfModifier(object):
-    """ Readability surf rdf export
+    """ Readability scores surf rdf export
     """
 
     implements(ISurfResourceModifier)
@@ -30,37 +29,28 @@ class Readability2SurfModifier(object):
               </eea:textField>
             </eea:readabilityScores>
         """
+        anno = IAnnotations(self.context)
+        scores = anno.get('readability_scores')
         output = []
+        if not scores:
+            return output
+
         session = resource.session
         store = session.get_default_store()
 
-        DataItem = session.get_class(surf.ns.EEA.textField)
+        eea_surf = surf.ns.EEA
+        store.reader.graph.bind('eea', eea_surf, override=True)
+        details_progress = session.get_class(surf.ns.EEA.textField)
 
-        anno = IAnnotations(self.context)
-        scores = anno.get('readability_scores')
         for key, val in scores.items():
-            d = DataItem(rdflib.BNode())
-            d.dcterms_title = key
-            d.eea_readabilityValue = val.get('readability_value', 1)
-            d.eea_wordCount = val.get('wordCount', 1)
-            d.eea_sentenceCount = val.get('sentenceCount', 1)
-            d.update()
+            rdfp = session.get_resource(key, details_progress)
+            rdfp[surf.ns.EEA['readabilityValue']] = val.get(
+                                                        'readability_value', 1)
+            rdfp[surf.ns.EEA['wordCount']] = val.get('wordCount', 1)
+            rdfp[surf.ns.EEA['sentenceCount']] = val.get('sentenceCount', 1)
+            rdfp.update()
+            output.append(rdfp)
 
-            output.append(d)
-
-
-        # eea_surf = surf.ns.EEA
-        # store.reader.graph.bind('eea', eea_surf, override=True)
-        # details_progress = session.get_class(surf.ns.EEA.textField)
-#
-        # for key, val in scores.items():
-        #     rdfp = session.get_resource(key, details_progress)
-        #     rdfp[surf.ns.EEA['readabilityValue']] = val.get('readability_value', 1)
-        #     rdfp[surf.ns.EEA['wordCount']] = val.get('wordCount', 1)
-        #     rdfp[surf.ns.EEA['sentenceCount']] = val.get('sentenceCount', 1)
-        #     rdfp.update()
-        #     output.append(rdfp)
-#
         resource['eea_readabilityScores'] = output
         resource.save()
         return resource
