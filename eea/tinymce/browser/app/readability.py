dfa64e1508e47f6fc35790e42b354903617ab292
ichim-david <ichim.david@gmail.com>
2017-03-20 16:15:14 +0200

    Refs #82316 pylint fixes

--- eea/tinymce/browser/app/readability.py
+++ eea/tinymce/browser/app/readability.py
@@ -3,6 +3,7 @@
 from Products.Five import BrowserView
 from zope.annotation import IAnnotations
 import json
+import math
 
 
 class EEAReadabilityPlugin(BrowserView):
@@ -27,7 +28,8 @@ class EEAReadabilityPlugin(BrowserView):
     def get_scores(self):
         """ get_scores """
         scores = self.anno.get(self.key, {})
-        key_metrics = {'word_count': 0, 'sentence_count': 0, 'readability_value': 0}
+        key_metrics = {'word_count': 0, 'sentence_count': 0,
+                       'readability_value': 0}
         enabled_for = 0
         for value in scores.values():
             if not value.get('readability_value'):
@@ -35,9 +37,13 @@ class EEAReadabilityPlugin(BrowserView):
             enabled_for += 1
             key_metrics['word_count'] += int(value.get('word_count', 0))
             key_metrics['sentence_count'] += value.get('sentence_count', 0)
-            key_metrics['readability_value'] += float(value.get('readability_value', 0))
-        # make an average score when we have more than 1 text field for which readability is enabled
+            key_metrics['readability_value'] += float(value.get(
+                'readability_value', 0))
+        # make an average score when we have more than 1 text field for
+        # which readability is enabled
         if enabled_for > 1:
-            key_metrics['readability_value'] = "{0:.2f}".format(key_metrics['readability_value'] / enabled_for)
+            key_metrics['readability_value'] = "{0:.2f}".format(
+                            key_metrics['readability_value'] / enabled_for)
         return key_metrics
+
 
