f7034d19bbd8da2e7e5ca43d51b07fc01ba1e4cb
David Ichim <ichim.david@gmail.com>
2019-11-14 16:40:47 +0200

    Refs #110784 added tile section templates as well as fullwidth grey section

--- eea/tinymce/skins/eea_tinymce_plugins/eeatemplateinit/editor_plugin.js
+++ eea/tinymce/skins/eea_tinymce_plugins/eeatemplateinit/editor_plugin.js
@@ -20,7 +20,12 @@
 
         loadContent: function (ed) {
             var tinyMCETemplateList = [
-                {title: "Figure", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/figure", decription:"Figure title and image"}
+                {title: "Figure", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/figure", decription:"Figure title and image"},
+                {title: "One tile", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-1-item", decription:"Fullwidth section with one tile content"},
+                {title: "Two tiles", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-2-items", decription:"Fullwidth section with two tiles content"},
+                {title: "Three tiles", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-3-items", decription:"Fullwidth section with three tiles content"},
+                {title: "Four tiles", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-4-items", decription:"Fullwidth section with four tiles content"},
+                {title: "Fullwidth grey section", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/fullwidth-grey-section", decription:"Fullwidth grey background section"}
             ];
             ed.settings.template_templates = window.globalTinyMCETemplateList || tinyMCETemplateList;
         }
