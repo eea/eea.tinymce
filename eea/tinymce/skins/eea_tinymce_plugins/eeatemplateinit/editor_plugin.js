131324333d4868ed9ec67e3a55b041b0441ad662
David Ichim <ichim.david@gmail.com>
2019-11-25 11:33:00 +0200

    Refs #110784 added pullquote templates and fixed https template loading

--- eea/tinymce/skins/eea_tinymce_plugins/eeatemplateinit/editor_plugin.js
+++ eea/tinymce/skins/eea_tinymce_plugins/eeatemplateinit/editor_plugin.js
@@ -20,13 +20,16 @@
 
         loadContent: function (ed) {
             var tinyMCETemplateList = [
-                {title: "Figure", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/figure", decription:"Figure title and image"},
-                {title: "One tile", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-1-item", decription:"Fullwidth section with one tile content"},
-                {title: "Two tiles", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-2-items", decription:"Fullwidth section with two tiles content"},
-                {title: "Three tiles", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-3-items", decription:"Fullwidth section with three tiles content"},
-                {title: "Four tiles", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-4-items", decription:"Fullwidth section with four tiles content"},
-                {title: "Fullwidth grey section", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/fullwidth-grey-section", decription:"Fullwidth grey background section"},
-                {title: "Pullquote full grey", src: "/www/SITE/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/pullquote-full-grey", decription:"Inline quote with author on grey background"}
+                {title: "Figure", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/figure", decription:"Figure title and image"},
+                {title: "One tile", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-1-item", decription:"Fullwidth section with one tile content"},
+                {title: "Two tiles", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-2-items", decription:"Fullwidth section with two tiles content"},
+                {title: "Three tiles", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-3-items", decription:"Fullwidth section with three tiles content"},
+                {title: "Four tiles", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-4-items", decription:"Fullwidth section with four tiles content"},
+                {title: "Fullwidth grey section", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/fullwidth-grey-section", decription:"Fullwidth grey background section"},
+                {title: "Fullwidth image section", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/fullwidth-image-container", decription:"Fullwidth image center section"},
+                {title: "Pullquote full", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/pullquote-full", decription:"Inline quote with author on grey background"},
+                {title: "Pullquote left", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/pullquote-left", decription:"Left pullquote with author on grey background"},
+                {title: "Pullquote Right", src: "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/pullquote-right", decription:"Right pullquote with author on grey background"}
             ];
             ed.settings.template_templates = window.globalTinyMCETemplateList || tinyMCETemplateList;
         }
