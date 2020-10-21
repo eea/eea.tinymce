fe65f011adb3be319586800fe193a3abddf241d4
David Ichim <ichim.david@gmail.com>
2020-10-21 13:01:57 +0300

    Refs #119674 changed the following:
    - editor_plugin.js:
      - fixed setting of template description, tweaking also the wording
      in order to avoid false statements

--- eea/tinymce/skins/eea_tinymce_plugins/eeatemplateinit/editor_plugin.js
+++ eea/tinymce/skins/eea_tinymce_plugins/eeatemplateinit/editor_plugin.js
@@ -23,73 +23,73 @@
           title: "Figure",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/figure",
-          decription: "Figure title and image"
+          description: "Figure title and image"
         },
         {
           title: "One tile",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-1-item",
-          decription: "Fullwidth section with one tile content"
+          description: "Fullwidth section with one tile content",
         },
         {
           title: "Two tiles",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-2-items",
-          decription: "Fullwidth section with two tiles content"
+          description: "Fullwidth section with two tiles content"
         },
         {
           title: "Three tiles",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-3-items",
-          decription: "Fullwidth section with three tiles content"
+          description: "Fullwidth section with three tiles content"
         },
         {
           title: "Four tiles",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/tiles-4-items",
-          decription: "Fullwidth section with four tiles content"
+          description: "Fullwidth section with four tiles content"
         },
         {
           title: "Fullwidth grey section",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/fullwidth-grey-section",
-          decription: "Fullwidth grey background section"
+          description: "Fullwidth grey background section"
         },
         {
           title: "Fullwidth image section",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/fullwidth-image-container",
-          decription: "Fullwidth image center section"
+          description: "Fullwidth image center section"
         },
         {
           title: "Fullwidth image",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/fullwidth-image",
-          decription: "Image spans entire browser width"
+          description: "Image spans entire browser width"
         },
         {
           title: "Max column image",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/max-container-image",
-          decription: "Image is max 1400px"
+          description: "Content is placed outside of content area, image is 100% width with max 1400px"
         },
         {
           title: "Pullquote full",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/pullquote-full",
-          decription: "Inline quote with author on grey background"
+          description: "Inline quote with author tag"
         },
         {
           title: "Pullquote left",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/pullquote-left",
-          decription: "Left pullquote with author on grey background"
+          description: "Left pullquote with author tag"
         },
         {
           title: "Pullquote Right",
           src:
             "/portal_skins/eea_tinymce_plugins/eeatemplateinit/templates/pullquote-right",
-          decription: "Right pullquote with author on grey background"
+          description: "Right pullquote with author tag"
         }
       ];
       ed.settings.template_templates =
